ef605b21d83a70432901dcefcea837602550c271
Alexey Krivchikov <krivchikovalexei@yandex.ru>
2023-03-01 18:12:52 +0300

    fix: сделал динамически-обновляемый год в footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,9 @@ import React from "react";
 function Footer() {
   return (
     <footer className="page__footer footer">
-      <p className="footer__copyright">&copy;&nbsp;2022 Mesto Russia</p>
+      <p className="footer__copyright">
+        &copy;&nbsp;{new Date().getFullYear()} Mesto Russia
+      </p>
     </footer>
   );
 }
